264945cd20a9c3e71d844be0d598b4171e6c0b37
alanv <alanvallve@gmail.com>
2018-02-26 19:43:14 +0100

    asgadg

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,52 @@
 var divs = [];
+var notaSend = "";
 
 var app = {
     initialize: function() {
-        this.bindEvents();
-        $("#create").click(crearNota());
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        $("#create").click(crearNota);
     }
 };
 
 function crearNota() {
-    var div = $("<div class='row nota'>" +
-                    "<div class='row'><h1>"+ $("#title").val() +"</h1></div>"+
+    var div = $("<div class='row no" +
+        "ta'>" +
+                    "<div class='row'><h1>"+ $("#title").val() +"</h1></div><br><br><br><br>"+
                     "<div class='row'>" + $("#desc").val() + "</div>"+
-                    "<div class='btn btn-warning editar'>editar</div><div class='btn btn-danger delete'>eliminar</div> "+
-                "</div>");
+                    "<div class='btn btn-warning editar'>editar</div><div class='btn btn-danger delete'>eliminar</div></div> <br>");
 
-    $("#notas").appendChild(div);
+    notaSend = div
+    $("#notas").append(div);
+    sendNotaAjax();
 }
 
-function sendNomAjax(){
-    var nomInput=$("#inputNom").val();
+function sendNotaAjax(){
+    var send = notaSend.get(0).outerHTML
     $.ajax({
         //type: "POST",
         dataType: "jsonp",
         jsonp: "callback",
-        url: "http://localhost:8080/api/load",
+        url: "http://localhost:8080/save",
+        data: {nota: send},
+        beforeSend: function () {
+            $("#respAjax").html('Connecting...');
+        },
+        success: function (respJSON) {
+            var codi = respJSON.nombre;
+            $("#respAjax").html(codi+" Longitud:"+respJSON.longitud);
+        }
+    });
+
+}
+
+function sendNotaByUrl(){
+    var nomInput=$("#inputNom").val();
+    var send = notaSend.get(0).outerHTML
+    console.log("http://localhost:8080/save/"+ send)
+    $.ajax({
+        //type: "POST",
+        dataType: "jsonp",
+        jsonp: "callback",
+        url: "http://localhost:8080/save/"+ send,
         data: {nombre: nomInput},
         beforeSend: function () {
             $("#respAjax").html('Connecting...');
