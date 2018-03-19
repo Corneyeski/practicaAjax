7c55bc01813898248cbd52219f3219fb6a34ce1c
alanv <alanvallve@gmail.com>
2018-03-19 19:38:03 +0100

    asgadg

--- www/js/index.js
+++ www/js/index.js
@@ -13,11 +13,22 @@ function crearNota() {
         "ta'>" +
                     "<div class='row'><h1>"+ $("#title").val() +"</h1></div><br><br><br><br>"+
                     "<div class='row'>" + $("#desc").val() + "</div>"+
-                    "<div class='btn btn-warning editar'>editar</div><div class='btn btn-danger delete'>eliminar</div></div> <br>");
+                    "</div> <br>");
 
-    notaSend = div
-    $("#notas").append(div);
+    notaSend = div;
+
     sendNotaAjax();
+
+    var editar = $("<div class='btn btn-warning editar'>editar</div>");
+    editar.click(edit);
+
+    var eliminar = $("<div class='btn btn-danger delete'>eliminar</div>");
+    eliminar.click(remove);
+
+    div.append(editar);
+    div.append(eliminar);
+
+    $("#notas").append(div);
 }
 
 function sendNotaAjax(){
@@ -38,7 +49,6 @@ function sendNotaAjax(){
             $("#respAjax").html(codi+" Longitud:"+respJSON.longitud);
         }
     });
-
 }
 
 function recoverNotasAjax(){
@@ -51,28 +61,53 @@ function recoverNotasAjax(){
         },
         success: function (respJSON) {
             console.log(respJSON)
+
+            for (var i = 0; i < respJSON.length; i++){
+
+                var div = $(respJSON[i]);
+
+                var editar = $("<div class='btn btn-warning editar' id='" + respJSON[i].substr(respJSON[i].length - 1) + "'>editar</div>");
+                editar.click(edit);
+
+                var eliminar = $("<div class='btn btn-danger delete' id='" + respJSON[i].substr(respJSON[i].length - 1) + "'>eliminar</div>");
+                eliminar.click(remove);
+
+                div.append(editar);
+                div.append(eliminar);
+
+                $("#notas").append(div);
+
+            }
         }
     });
 
 }
 
-/*function sendNotaByUrl(){
-    var nomInput=$("#inputNom").val();
-    var send = notaSend.get(0).outerHTML
-    console.log("http://localhost:8080/save/"+ send)
+function removeNota(id){
+
     $.ajax({
-        //type: "POST",
-        dataType: "jsonp",
-        jsonp: "callback",
-        url: "http://localhost:8080/save/"+ send,
-        data: {nombre: nomInput},
+        type: "POST",
+        dataType: "json",
+        contentType:"text/plain",
+        url: "http://localhost:8080/remove/"+id,
         beforeSend: function () {
             $("#respAjax").html('Connecting...');
         },
         success: function (respJSON) {
-            var codi = respJSON.nombre;
-            $("#respAjax").html(codi+" Longitud:"+respJSON.longitud);
+            console.log(respJSON[0])
         }
     });
+}
 
-}*/
+function remove() {
+    console.log(this.getAttribute("id"));
+    removeNota($(this).attr("id"));
+
+    $(this).parent("div").remove();
+    $(this).remove();
+    console.log("eliminar");
+}
+
+function edit() {
+    console.log("editar");
+}
